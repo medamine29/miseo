4b02724276c1218538be68c1e33d3cc49c707d9a
Mohamed Amine <mohamedamine.naffeti2@esprit.tn>
2021-11-30 00:48:38 +0100

    init project

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget{
+
+  MyApp._internal(); // private named constructor
+
+  
+  static final MyApp instance = MyApp._internal(); // applying singleton on App instance
+
+  factory MyApp() => instance; // factory for the class instance --> always return the same instance
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
