bd1c2e6ff7a58c26a2587c691d7c32f966014644
Mohamed Amine <mohamedamine.naffeti2@esprit.tn>
2021-12-02 01:25:46 +0100

    adding splash screen + timer

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miseo/presentation/resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -10,6 +11,13 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.darkPrimary,
+      body: Center(
+        child: Text(
+          "Welcome to unboarding",
+        ),
+      ),
+    );
   }
 }
