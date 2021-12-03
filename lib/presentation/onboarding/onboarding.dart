be868dc8c0c6bac56badf5a77eb96cabce96bab0
Mohamed Amine <mohamedamine.naffeti2@esprit.tn>
2021-12-03 01:25:29 +0100

    onboarding screen + adding assets

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:miseo/presentation/resources/assets_manager.dart';
 import 'package:miseo/presentation/resources/color_manager.dart';
+import 'package:miseo/presentation/resources/strings_manager.dart';
+import 'package:miseo/presentation/resources/values_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,15 +14,113 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+
+  PageController _pageController = PageController(initialPage: 0);
+
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onBoardingTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onBoardingTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onBoardingTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onBoardingTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorManager.darkPrimary,
-      body: Center(
-        child: Text(
-          "Welcome to unboarding",
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return OnBoardingPage(_list[index]);
+        },
+      ),
+      bottomSheet: Container(
+        color: ColorManager.white,
+        height: AppSize.s100,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  AppStrings.skip,
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ),
+            // add Layout for indicator and arrows
+          ],
         ),
       ),
     );
   }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  SliderObject _sliderObject;
+  OnBoardingPage(this._sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: AppSize.s40,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        SizedBox(
+          height: AppSize.s60,
+        ),
+        //image widget
+        SvgPicture.asset(_sliderObject.image),
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -30,5 +30,7 @@ class AppSize{
   static const double s20 = 20.0;
   static const double s24 = 24.0;
   static const double s28 = 28.0;
+  static const double s40 = 40.0;
   static const double s60 = 60.0;
+  static const double s100 = 100.0;
 }
